10c9e9474c50aba750027522c4aa1071950c56c8
Öznur <oznur@tuta.io>
2022-05-11 22:37:53 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("submit", () => {
+    document.querySelector("#tasks").innerHTML += `
+    <div class="task">
+        <span id="taskname">
+            ${document.querySelector("#create-task-form input").value}
+        </span>
+        <button class="delete">
+            <i class="far fa-trash-alt"></i>
+        </button>
+    </div>
+  `;
+
+    var current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+    event.preventDefault();
+  });
 });
